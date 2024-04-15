dc9002265c38c8b367a88529b7b3bf1b7c27701c
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-15 19:47:20 -0300

    feat: add login image

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <img :src="imageSrc" class="w-7/12 h-screen" alt="login-image">
+</template>
+
+<script>
+import login from '@/assets/images/login-image.png'
+
+export default {
+  name: "App",
+    // components: { Header, Menu, Content, Footer, Loading },
+    // computed: mapState(['isMenuVisible', 'user']),
+    data: function() {
+      return {
+        imageSrc: login
+      }
+    },
+  }
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
