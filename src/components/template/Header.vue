8b78af72423243a2293972a872166bd43ce8fc8c
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 14:10:10 -0300

    feat: add labels to Header

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="h-20 flex items-center bg-lochmara px-6">
     <img :src="imageSrc" class="w-10 h-10" alt="menu-icon">
+    <div class="flex justify-between grow">
+      <span class="text-2xl text-white font-black capitalize">utilitários</span>
+      <span class="text-2xl text-white font-black capitalize">manager</span>
+    </div>
   </div>
 </template>
 
