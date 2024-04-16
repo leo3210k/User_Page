1091e8ce8b3e23585228d462d89e58ca2783af78
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 17:52:12 -0300

    style: add col span on Header

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-20 flex items-center bg-lochmara px-6 gap-x-14">
+  <div class="col-span-2 flex items-center bg-lochmara px-6 gap-x-14">
     <img :src="imageSrc" class="w-10 h-10" alt="menu-icon">
     <div class="flex justify-between text-2xl text-white font-black capitalize grow">
       <span>utilitários</span>
