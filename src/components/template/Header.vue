f48fe27547dc33238ce30f28e3c9d1df2fddb551
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 14:05:07 -0300

    feat: add menu hamburguer to Header

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="h-20 bg-lochmara">
-    
+    <img :src="imageSrc" class="w-10 h-10" alt="menu-icon">
   </div>
 </template>
 
 <script>
+import menu_hamburguer from '@/assets/svg/menu-hamburguer.svg'
+
 export default {
   name: "Header",
+  data() {
+    return {
+      imageSrc: menu_hamburguer
+    };
+  }
 }
 </script>
 
