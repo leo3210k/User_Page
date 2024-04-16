ab48e17af7f81cae4fd65c31f1ed828de5a6cf44
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 14:14:13 -0300

    chore: add SideMenu to Home component

--- src/components/template/Home.vue
+++ src/components/template/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <Header />
+  <SideMenu />
 </template>
 
 <script>
 import Header from '@/components/template/Header.vue'
+import SideMenu from '@/components/template/SideMenu.vue'
 
 export default {
   name: "Home",
-  components: { Header }
+  components: { Header, SideMenu }
 }
 </script>
 
