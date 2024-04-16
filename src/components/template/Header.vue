666fe2e16347bbcc1ee1b892d1f1fb48d3110235
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 14:12:35 -0300

    style: add gap between labels and menu icon

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-20 flex items-center bg-lochmara px-6">
+  <div class="h-20 flex items-center bg-lochmara px-6 gap-x-14">
     <img :src="imageSrc" class="w-10 h-10" alt="menu-icon">
     <div class="flex justify-between text-2xl text-white font-black capitalize grow">
       <span>utilitários</span>
