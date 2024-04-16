d59197dddd4186f8d19373b238f0807acbe66741
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 08:36:33 -0300

    add form shadcn component to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,23 +3,54 @@
     <img :src="imageSrc" class="w-7/12 h-screen" alt="login-image">
     <div class="flex flex-col text-4xl font-bold text-black m-auto">
       <h1>Login</h1>
+      <form @submit="onSubmit">
+        <FormField v-slot="{ componentField }" name="username">
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input type="text" placeholder="shadcn" v-bind="componentField" />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <Button type="submit">
+          Submit
+        </Button>
+      </form>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import login from '@/assets/images/login-image.png'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+import { useForm } from 'vee-validate'
+import { Button } from '@/components/ui/button'
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 
-export default {
-  name: "App",
-    // components: { Header, Menu, Content, Footer, Loading },
-    // computed: mapState(['isMenuVisible', 'user']),
-    data: function() {
-      return {
-        imageSrc: login
-      }
-    },
-  }
+const formSchema = toTypedSchema(z.object({
+  username: z.string().min(2).max(50),
+}))
+
+const form = useForm({
+  validationSchema: formSchema,
+})
+
+const onSubmit = form.handleSubmit((values) => {
+  console.log('Form submitted!', values)
+})
 </script>
 
 <style scoped>
