c51d9a9f644d9d316c60e8a880a794a8cacbfc24
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 17:44:20 -0300

    chore: create PageModel component

---
+++ src/components/template/PageModel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+    <SideMenu />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/template/Header.vue'
+import SideMenu from '@/components/template/SideMenu.vue'
+
+export default {
+  name: "Home",
+  components: { Header, SideMenu }
+}
+</script>
+
+<style>
+
+</style>
