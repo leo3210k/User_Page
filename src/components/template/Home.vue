97befcce67c0d939b8977a805103359adf6d519c
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 22:29:59 -0300

    style: add bold and text center on icons labels

--- src/components/template/Home.vue
+++ src/components/template/Home.vue
@@ -2,9 +2,9 @@
   <PageModel>
     <div class="flex flex-wrap gap-6 m-32">
       <div v-for="(image, index) in images" :key="index"
-      class="w-36 h-36 flex flex-col justify-center items-center bg-azure-radiance-darker rounded">
+      class="w-36 h-36 flex flex-col gap-3 justify-center items-center bg-azure-radiance-darker rounded">
         <img :src="image.src" :alt="image.src" class="w-14 h-14">
-        <span>{{ image.label }}</span>
+        <span class="text-base font-bold text-center">{{ image.label }}</span>
       </div>
     </div>
   </PageModel>
