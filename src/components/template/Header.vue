93b32c976385b5387e039533138b649294d40e78
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 21:38:08 -0300

    style: change from imageSrc to image_src on Header

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-span-2 flex items-center bg-lochmara px-6 gap-x-14">
-    <img :src="imageSrc" class="w-10 h-10" alt="menu-icon">
+    <img :src="image_src" class="w-10 h-10" alt="menu-icon">
     <div class="flex justify-between text-2xl text-white font-black capitalize grow">
       <span>utilitários</span>
       <span>manager</span>
@@ -15,7 +15,7 @@ export default {
   name: "Header",
   data() {
     return {
-      imageSrc: menu_hamburguer
+      image_src: menu_hamburguer
     };
   }
 }
