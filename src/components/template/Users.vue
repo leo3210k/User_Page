0ada5283c2ae751238766d2156477c9de2f7a51e
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 22:58:12 -0300

    style: add no users stylization

--- src/components/template/Users.vue
+++ src/components/template/Users.vue
@@ -5,7 +5,7 @@
         <span class="text-black text-2xl font-medium">Usuários</span>
         <div class="w-10 h-10 flex justify-center items-center text-4xl bg-azure-radiance-darker text-white rounded-md">+</div>
       </div>
-      <div>
+      <div class="flex flex-col justify-center items-center gap-6 text-gray-500">
         <img :src="image_src" class="w-64 h-64" :alt="image_src">
         <span>Não há usuários. Clique no ícone acima para adicionar</span>
       </div>
