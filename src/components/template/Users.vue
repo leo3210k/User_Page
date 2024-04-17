46080bb72667c2d4e208a0fd0deeba56862fdaef
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 22:57:30 -0300

    feat: add no users image

--- src/components/template/Users.vue
+++ src/components/template/Users.vue
@@ -5,18 +5,21 @@
         <span class="text-black text-2xl font-medium">Usuários</span>
         <div class="w-10 h-10 flex justify-center items-center text-4xl bg-azure-radiance-darker text-white rounded-md">+</div>
       </div>
+      <img :src="image_src" class="w-64 h-64" :alt="image_src">
     </div>
   </PageModel>
 </template>
 
 <script>
 import PageModel from '@/components/template/PageModel.vue'
+import no_users from '@/assets/images/no-users.png'
 
 export default {
   name: "Users",
   components: { PageModel },
   data() {
     return {
+      image_src: no_users,
     };
   }
 }
