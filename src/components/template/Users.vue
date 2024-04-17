ca5d50a0b328be2e321aceb34ac4a474de59c875
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 22:57:47 -0300

    add no users label

--- src/components/template/Users.vue
+++ src/components/template/Users.vue
@@ -5,7 +5,10 @@
         <span class="text-black text-2xl font-medium">Usuários</span>
         <div class="w-10 h-10 flex justify-center items-center text-4xl bg-azure-radiance-darker text-white rounded-md">+</div>
       </div>
-      <img :src="image_src" class="w-64 h-64" :alt="image_src">
+      <div>
+        <img :src="image_src" class="w-64 h-64" :alt="image_src">
+        <span>Não há usuários. Clique no ícone acima para adicionar</span>
+      </div>
     </div>
   </PageModel>
 </template>
