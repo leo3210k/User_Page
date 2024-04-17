a33ad4b775933ac95391afc13128e5cf10590042
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 22:34:22 -0300

    feat: add PageModel to Users

--- src/components/template/Users.vue
+++ src/components/template/Users.vue
@@ -1,10 +1,13 @@
 <template>
-
+  <PageModel />
 </template>
 
 <script>
+import PageModel from '@/components/template/PageModel.vue'
+
 export default {
   name: "Users",
+  components: { PageModel },
   data() {
     return {
     };
