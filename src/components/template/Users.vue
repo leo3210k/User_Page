c37496d1c1b65461734e186062e50debc023077a
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-16 22:58:48 -0300

    style: change no users to absolute and center

--- src/components/template/Users.vue
+++ src/components/template/Users.vue
@@ -1,11 +1,11 @@
 <template>
   <PageModel>
-    <div class="m-10">
+    <div class="relative flex flex-col m-10">
       <div class="flex items-center gap-6">
         <span class="text-black text-2xl font-medium">Usuários</span>
         <div class="w-10 h-10 flex justify-center items-center text-4xl bg-azure-radiance-darker text-white rounded-md">+</div>
       </div>
-      <div class="flex flex-col justify-center items-center gap-6 text-gray-500">
+      <div class="absolute inset-1 flex flex-col justify-center items-center gap-6 text-gray-500">
         <img :src="image_src" class="w-64 h-64" :alt="image_src">
         <span>Não há usuários. Clique no ícone acima para adicionar</span>
       </div>
