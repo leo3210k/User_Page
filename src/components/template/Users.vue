d6d2cdc15501eb0229d70731392e9f85661da7e8
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-17 22:59:26 -0300

    feat: import UsersDialog

--- src/components/template/Users.vue
+++ src/components/template/Users.vue
@@ -3,7 +3,7 @@
     <div class="relative flex flex-col m-10">
       <div class="flex items-center gap-6">
         <span class="text-black text-2xl font-medium">Usuários</span>
-        <div class="w-10 h-10 flex justify-center items-center text-4xl bg-azure-radiance-darker text-white rounded-md">+</div>
+        <UsersDialog />
       </div>
       <div class="absolute inset-1 flex flex-col justify-center items-center gap-6 text-gray-400">
         <img :src="image_src" class="w-64 h-64" :alt="image_src">
@@ -15,11 +15,12 @@
 
 <script>
 import PageModel from '@/components/template/PageModel.vue'
+import UsersDialog from '@/components/template/UsersDialog.vue'
 import no_users from '@/assets/images/no-users.png'
 
 export default {
   name: "Users",
-  components: { PageModel },
+  components: { PageModel, UsersDialog },
   data() {
     return {
       image_src: no_users,
