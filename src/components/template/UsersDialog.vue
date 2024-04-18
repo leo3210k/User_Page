e25b2b81f559b24c3f30b0481acc1130e44f9654
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-17 23:04:52 -0300

    feat: create dialog

--- src/components/template/UsersDialog.vue
+++ src/components/template/UsersDialog.vue
@@ -1,31 +1,14 @@
 <template>
   <Dialog>
     <DialogTrigger as-child>
-      <Button variant="outline">
-        Edit Profile
+      <Button class="w-10 h-10 flex justify-center items-center text-4xl bg-azure-radiance-darker hover:bg-azure-radiance-darker hover:opacity-85 text-white rounded-md z-10">
+        +
       </Button>
     </DialogTrigger>
-    <DialogContent class="sm:max-w-[425px] grid-rows-[auto_minmax(0,1fr)_auto] p-0 max-h-[90dvh]">
-      <DialogHeader class="p-6 pb-0">
-        <DialogTitle>Edit profile</DialogTitle>
-        <DialogDescription>
-          Make changes to your profile here. Click save when you're done.
-        </DialogDescription>
+    <DialogContent class="sm:max-w-md">
+      <DialogHeader>
+        <DialogTitle class="text-black">Share link</DialogTitle>
       </DialogHeader>
-      <div class="grid gap-4 py-4 overflow-y-auto px-6">
-        <div class="flex flex-col justify-between h-[300dvh]">
-          <p>
-            This is some placeholder content to show the scrolling behavior for modals. We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
-          </p>
-
-          <p>This content should appear at the bottom after you scroll.</p>
-        </div>
-      </div>
-      <DialogFooter class="p-6 pt-0">
-        <Button type="submit">
-          Save changes
-        </Button>
-      </DialogFooter>
     </DialogContent>
   </Dialog>
 </template>
