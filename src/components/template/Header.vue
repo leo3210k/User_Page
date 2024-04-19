80e648946b9f1c322596d112668e44614155f22d
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-19 16:48:32 -0300

    style: add cursor pointer to menu button

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-span-2 flex items-center bg-lochmara px-6 gap-x-14">
-    <img @click="toggleMenu" :src="image_src" class="w-10 h-10" alt="menu-icon">
+    <img @click="toggleMenu" :src="image_src" class="w-10 h-10 cursor-pointer" alt="menu-icon">
     <div class="flex justify-between text-2xl text-white font-black capitalize grow">
       <div>
         <span>utilitários</span>
