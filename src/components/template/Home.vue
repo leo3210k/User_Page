03e9b3bcdc9fe7c4167d9f9a1d37f0ca1f37ad4c
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-19 16:51:58 -0300

    style: add hover on users button

--- src/components/template/Home.vue
+++ src/components/template/Home.vue
@@ -2,7 +2,7 @@
   <PageModel>
     <div class="h-40 flex flex-wrap gap-6 m-32" :class="isMenuVisible ? '' : 'col-span-2' ">
       <div v-for="(image, index) in images" :key="index">
-        <RouterLink to="/users" v-if="image.label=='Usuários'" class="w-36 h-36 flex flex-col gap-3 justify-center items-center bg-azure-radiance-darker rounded-lg">
+        <RouterLink to="/users" v-if="image.label=='Usuários'" class="w-36 h-36 flex flex-col gap-3 justify-center items-center bg-azure-radiance-darker hover:bg-azure-radiance-darker/80 rounded-lg">
           <img :src="image.src" :alt="image.src" class="w-14 h-14">
           <span class="text-base font-bold text-center">{{ image.label }}</span>
         </RouterLink>
