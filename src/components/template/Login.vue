ce666cc9c51009090c9a7d75c8696c169f2593a7
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-19 16:34:47 -0300

    feat: import axios

--- src/components/template/Login.vue
+++ src/components/template/Login.vue
@@ -50,6 +50,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import axios from 'axios'
 
 const formSchema = toTypedSchema(z.object({
   email: z.string(),
