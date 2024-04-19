ff2fed3b588c08ec7d172765d4344577056c5032
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-19 16:30:18 -0300

    style: fix date input placeholder

--- src/components/template/UsersDialog.vue
+++ src/components/template/UsersDialog.vue
@@ -98,7 +98,7 @@
                       <Button
                         class="w-full h-12 bg-white text-black hover:bg-white border"
                       >
-                        <span>{{ value ? df.format(toDate(value)) : "Pick a date" }}</span>
+                        <span>{{ value ? df.format(toDate(value)) : "Selecione uma data" }}</span>
                         <CalendarIcon class="ms-auto h-4 w-4 opacity-50" />
                       </Button>
                       <input hidden>
