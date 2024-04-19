84848ee9676b7c36479ea223d8d8fefe18ae231d
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-19 16:41:18 -0300

    feat: add breadcrumb to Header

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -2,7 +2,11 @@
   <div class="col-span-2 flex items-center bg-lochmara px-6 gap-x-14">
     <img :src="image_src" class="w-10 h-10" alt="menu-icon">
     <div class="flex justify-between text-2xl text-white font-black capitalize grow">
-      <span>utilitários</span>
+      <div>
+        <span>utilitários</span>
+        /
+        <span>Usuários</span>
+      </div>
       <span>manager</span>
     </div>
   </div>
